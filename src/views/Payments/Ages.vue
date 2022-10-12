097080c0f836329b5a7703c3d3419bab0c6ce8da
Yerson Muñoz Erazo <jmerazo96@gmail.com>
2022-10-11 21:35:45 -0500

    report ages table

--- src/views/Payments/Ages.vue
+++ src/views/Payments/Ages.vue
@@ -4,7 +4,7 @@
             <div class="row" id="form-search-parameters">
                 <div class="col-4" id="align-text-fai">
                     <font-awesome-icon id="fai-search" :icon="['fas', 'calendar']"/><label class="form-label">Select date</label>
-                    <v-date-picker class="inline-block h-full" v-model="date" :masks="masks" mode='date' data="YYYY-MM-DD">
+                    <v-date-picker class="inline-block h-full" v-model="date" :masks="masks" mode='date' data="YYYY-MM-DD" :format="YYYY-MM-DD">
                         <template v-slot="{ inputValue, togglePopover }">
                             <div class="flex items-center">
                                 <button
@@ -31,7 +31,7 @@
                     </v-date-picker>
                 </div>
 
-                {{date}}
+                {{this.date}}
 
                 <div class="col-2">
                     <a id="btn-search-report" type="submit" class="btn" @click="searchParameters">Search</a>
@@ -40,9 +40,42 @@
            
             <a id="btn-search-export" title="Export report" type="submit" class="btn" @click="expReport"><font-awesome-icon id="fai-export" :icon="['fas', 'file-export']"/></a> 
             <div class="row" id="form-label-input">
-                <img src="@/assets/resources/ESE.jpg" alt="Log ESE HJMH" id="img-report-ages">
+                <img src="@/assets/resources/logoese.png" alt="Log ESE HJMH" id="img-report-ages">
                 <h2 id="register-title" class="font-bold text-2xl">E.S.E Hospital José María Hernández</h2>
-                <label id="register-subtitle" class="font-semibold text-lg">Nit: 891.200.679-1</label><br>    
+                <label id="register-subtitle" class="font-semibold text-lg">Nit: 891.200.679-1</label><br><br>
+                <span class="font-semibold text-lg">Informe de cuentas por cobrar de empresas deudoras vencidas</span>
+                <table class="table table table-striped table-hover">
+                    <thead class="thead-dark">
+                      <tr id="tr-title">
+                          <th>Empresa / Nit</th>
+                          <th>No Vencida</th>
+                          <th>1 a 30 días</th>
+                          <th>31 a 60 días</th>
+                          <th>61 a 90 días</th>
+                          <th>91 a 180 días</th>
+                          <th>181 a 360 días</th>
+                          <th>Más de 360 días</th>
+                          <th>Saldo por cobrar</th>
+                          <th>Glosas por cobrar</th>
+                          <th>Por conciliar</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="a in ages" :value="a.nit" :key="a.nit">
+                          <td>{{a.nombre +" - Nit: "+a.nit}}</td>
+                          <td>{{a.edad0}}</td>
+                          <td>{{a.edad1}}</td>
+                          <td>{{a.edad2}}</td>
+                          <td>{{a.edad3}}</td>
+                          <td>{{a.edad4}}</td>
+                          <td>{{a.edad5}}</td>
+                          <td>{{a.edad6}}</td>
+                          <td>{{'p'}}</td>
+                          <td>{{'p'}}</td>
+                          <td>{{'p'}}</td>
+                      </tr>
+                    </tbody>
+                  </table> 
             </div>        
         </div>     
     </section>     
@@ -65,12 +98,13 @@ data() {
         nit: null,
         nits: null,
         sale: null,
-        search: [],
+        ages: [],
         date_init: null,
         date_end: null
     };    
 },
 mounted() {
+    this.agesReport();
 },
 computed: {
 },
@@ -82,15 +116,15 @@ methods: {
         XLSX.utils.book_append_sheet(workbook, data, filename);
         XLSX.writeFile(workbook, `${filename}.xlsx`);
     },
-    async searchNit() {
-        await axios.get(`http://localhost:8844/api/payments/nit/${this.nit}`, {
+    async agesReport() {
+        await axios.get(`http://localhost:8844/api/report/ages/`, {
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*"
         }
         })
         .then((Response) => {
-            this.search = Response.data;
+            this.ages = Response.data;
         });
     }          
 }
@@ -105,8 +139,10 @@ methods: {
 }
 
 #img-report-ages{
-    width: 80px;
-    height: 45px;
+    position: absolute;
+    width: 100px;
+    height: 55px;
+    top: 180px;
 }
 
 #btn-search-export{
